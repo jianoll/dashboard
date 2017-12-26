30f89efd8d6ae88f2fdda7094f48d5fc859833ce
xianhua <xianhua@meizu.com>
2017-12-26 15:17:15 +0800

    'init'

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-
-- install
-```
-    wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-4.6.3-1.x86_64.rpm 
-    sudo yum localinstall grafana-4.6.3-1.x86_64.rpm 
-    #grafana-cli plugins list-remote
-    
-    grafana-cli plugins install stagemonitor-elasticsearch-app 
-    grafana-cli plugins install grafana-openfalcon-datasource 
-    grafana-cli plugins install raintank-kubernetes-app 
-    grafana-cli plugins install abhisant-druid-datasource 
-    grafana-cli plugins install foursquare-clouderamanager-datasource 
-    grafana-cli plugins install praj-ams-datasource
-    grafana-cli plugins install crate-datasource
-    grafana-cli plugins install percona-percona-app 
-    grafana-cli plugins install alexanderzobnin-zabbix-app
-```
-
 - grafana dashboards
 ```
     https://grafana.com/dashboards/2292
@@ -32,22 +14,21 @@
     https://grafana.com/dashboards/405
     https://grafana.com/dashboards/315
     https://grafana.com/dashboards/741
+    
     https://grafana.com/dashboards/3244
     https://grafana.com/dashboards/1124
     https://grafana.com/dashboards/1666
     https://grafana.com/dashboards/2322
-    https://grafana.com/dashboards/2347
-    https://grafana.com/dashboards/3107
     https://grafana.com/dashboards/3070
     https://grafana.com/dashboards/2525
     https://grafana.com/dashboards/2121
-    https://grafana.com/dashboards/1575
     https://grafana.com/dashboards/2113
     https://grafana.com/dashboards/3901
     https://grafana.com/dashboards/2040
     https://grafana.com/dashboards/2043
     https://grafana.com/dashboards/2037
     https://github.com/percona/grafana-dashboards/tree/master/dashboards
-    https://github.com/hammerlab/grafana-spark-dashboards
     https://grafana.com/dashboards/878
+    
+    https://github.com/hammerlab/grafana-spark-dashboards
 ```
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+yum install -y urw-fonts
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-4.6.3-1.x86_64.rpm
+sudo yum localinstall grafana-4.6.3-1.x86_64.rpm
+
+grafana-cli plugins install camptocamp-prometheus-alertmanager-datasource
+grafana-cli plugins install abhisant-druid-datasource
+grafana-cli plugins install foursquare-clouderamanager-datasource
+grafana-cli plugins install crate-datasource
+grafana-cli plugins install percona-percona-app
+grafana-cli plugins install alexanderzobnin-zabbix-app
+grafana-cli plugins install fastweb-openfalcon-datasource
+
+mkdir /var/lib/grafana/dashboards
+wget https://grafana.com/api/dashboards/1124/revisions/4/download -O /var/lib/grafana/dashboards/prometheus_snmp_network.json
+wget https://grafana.com/api/dashboards/1666/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_bind.json
+wget https://grafana.com/api/dashboards/2322/revisions/3/download -O /var/lib/grafana/dashboards/prometheus_elasticsearch.json
+wget https://grafana.com/api/dashboards/3070/revisions/3/download -O /var/lib/grafana/dashboards/prometheus_etcd.json
+wget https://grafana.com/api/dashboards/2525/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_logstash.json
+wget https://grafana.com/api/dashboards/2121/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_rabbitmq.json
+wget https://grafana.com/api/dashboards/2113/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_hystrix.json
+wget https://grafana.com/api/dashboards/3901/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_php-fpm.json
+wget https://grafana.com/api/dashboards/2040/revisions/4/download -O /var/lib/grafana/dashboards/prometheus_tidb.json
+wget https://grafana.com/api/dashboards/2043/revisions/8/download -O /var/lib/grafana/dashboards/prometheus_tikv.json
+wget https://grafana.com/api/dashboards/2037/revisions/4/download -O /var/lib/grafana/dashboards/prometheus_pd.json
+wget https://grafana.com/api/dashboards/159/revisions/4/download -O /var/lib/grafana/dashboards/prometheus.json
+wget https://grafana.com/api/dashboards/3457/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_jmx.json
+wget https://grafana.com/api/dashboards/2949/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_nginx_vts.json
+wget https://grafana.com/api/dashboards/926/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_ceph_pools.json
+wget https://grafana.com/api/dashboards/923/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_ceph_osd.json
+wget https://grafana.com/api/dashboards/917/revisions/1/download -O /var/lib/grafana/dashboards/prometheus_ceph_cluster.json
+
+wget https://grafana.com/api/dashboards/2030/revisions/1/download -O /var/lib/grafana/dashboards/system_metrics.json
+
+wget https://grafana.com/api/dashboards/2711/revisions/3/download -O /var/lib/grafana/dashboards/zabbix.json
+
+wget https://grafana.com/api/dashboards/3244/revisions/1/download -O /var/lib/grafana/dashboards/kubernetes_calico.json
+wget https://grafana.com/api/dashboards/741/revisions/1/download -O /var/lib/grafana/dashboards/kubernetes_deployment.json
+wget https://grafana.com/api/dashboards/315/revisions/3/download -O /var/lib/grafana/dashboards/kubernetes.json
+wget https://grafana.com/api/dashboards/405/revisions/6/download -O /var/lib/grafana/dashboards/kubernetes_node.json
+wget https://grafana.com/api/dashboards/1860/revisions/9/download -O /var/lib/grafana/dashboards/kubernetes_node_full.json
+
+wget https://grafana.com/api/dashboards/878/revisions/5/download -O /var/lib/grafana/dashboards/elasticsearch.json
+
+wget https://grafana.com/api/dashboards/2292/revisions/5/download -O /var/lib/grafana/dashboards/nginx_access_log.json
+
+wget https://github.com/percona/grafana-dashboards/archive/master.zip -O /tmp/percona.zip
+cd /tmp && unzip /tmp/percona.zip
+cp -r /tmp/grafana-dashboards-master/dashboards /var/lib/grafana/
+cp -r grafana-dashboards/dashboards /var/lib/grafana/
+
+
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable grafana-server.service
+sudo /bin/systemctl start grafana-server.service
